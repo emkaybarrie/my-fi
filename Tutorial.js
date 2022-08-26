52a544548e224b7b7d8faaeff55f624ac2b9512e
emkaybarrie <106494695+emkaybarrie@users.noreply.github.com>
2022-08-26 15:24:11 +0100

    Updated Title,Tutorial, and Kianova Screens - updated for E2E test of Title to Prologue level to Kianova

--- Tutorial.js
+++ Tutorial.js
@@ -1,11 +1,28 @@
+var tutorialScreenRandomiser
 var objectiveText
-var controlsText1
-var controlsText2
+var controlsText
 var controlsBackground
 var hintText
 var hintTextContent
+var hintTextRandomiser
 var diagram
+var lStick
+var rTrigger
+var lTrigger
+var rBumper
+var lBumper
+var gamePadControls
+
 var diagram2
+var lArrow
+var rArrow
+var uArrow
+var dArrow
+var aButton
+var dButton
+var spaceButton
+var fButton
+var keyboardControls
 
 var startText
 var nextScene
@@ -19,34 +36,45 @@ class Tutorial extends Phaser.Scene {
         super("Tutorial")
     }
 
-    functionAccessTest(game){
-        game.tweens.add({
-            delay: 0,
-            targets: [lvlBG1,lvlBG2],
-            alpha: { value: 0, duration: 1000, ease: 'Power1'}
-    
-        });
-    
-        nightBorne.setDragX(0)
-        game.physics.moveTo(nightBorne, width * 3, 0,400)
-    }
+
 
 
 
     preload(){
-        this.load.image('tutorialA', 'assets/TutorialScreenA.png');
-        this.load.image('gamePadTut', 'assets/controls/gamePadTut2.png');
+        var tutorialScreenRandomiser = Phaser.Math.Between(0,3)
+        if(tutorialScreenRandomiser == 0){
+            this.load.image('tutorialScreen', 'assets/TutorialScreenA.png');
+        } else if (tutorialScreenRandomiser == 1){
+            this.load.image('tutorialScreen', 'assets/TutorialScreenB.png');
+        } else if (tutorialScreenRandomiser == 2){
+            this.load.image('tutorialScreen', 'assets/TutorialScreenC.png');
+        } else if (tutorialScreenRandomiser == 3){
+            this.load.image('tutorialScreen', 'assets/TutorialScreenD.png');
+        } 
+        this.load.image('gamePadTut', 'assets/controls/gamePadTut1.png');
+        this.load.image('lStick', 'assets/controls/LS.png');
+        this.load.image('lTrigger', 'assets/controls/LT.png');
+        this.load.image('lBumper', 'assets/controls/LB.png');
+        this.load.image('rTrigger', 'assets/controls/RT.png');
+        this.load.image('rBumper', 'assets/controls/RB.png');
         this.load.image('keyBoardTut', 'assets/controls/keyBoardTut1.jpg');
+        this.load.image('lArrow', 'assets/controls/ARROWLEFT.png');
+        this.load.image('rArrow', 'assets/controls/ARROWRIGHT.png');
+        this.load.image('uArrow', 'assets/controls/ARROWUP.png');
+        this.load.image('dArrow', 'assets/controls/ARROWDOWN.png');
+        this.load.image('aButton', 'assets/controls/A.png');
+        this.load.image('dButton', 'assets/controls/D.png');
+        this.load.image('spaceButton', 'assets/controls/SPACE.png');
+        this.load.image('fButton', 'assets/controls/F.png');
+
 
     }
 
     create(){
         camera = this.cameras.main.fadeIn(2000)
-        controlsBackground = this.add.rectangle(0,350,width,250,0x800080).setOrigin(0,0).setAlpha(0) //0xFFFFFF 0x800080
-        this.tutorialImage = this.add.image(0,0,'tutorialA').setScale(1,1.1).setOrigin(0,0)
+        controlsBackground = this.add.rectangle(0,275,width,425,0x800080).setOrigin(0,0).setAlpha(0) //0xFFFFFF 0x800080
+        this.tutorialImage = this.add.image(0,0,'tutorialScreen').setScale(1,1.1).setOrigin(0,0)
         
-        diagram = this.add.image(1075,400,'gamePadTut').setScale(0.15,0.15).setOrigin(0,0).setAlpha(0)
-        diagram2 = this.add.image(1525,425,'keyBoardTut').setScale(0.2,0.25).setOrigin(0,0).setAlpha(0)
 
         var hintTextRandomiser = Phaser.Math.Between(0,5)
 
@@ -74,11 +102,11 @@ class Tutorial extends Phaser.Scene {
             }
         });
 
-        controlsText1 = this.make.text({
+        controlsText = this.make.text({
             alpha: 0,
-            x: diagram.x + 300,
+            x: 1225,
             y: 385,
-            text: 'Left Stick: Movement\n\nRT: Offensive Action\nLT: Defensive Action\n\nRB: Skill 1\nLB: Skill 2',
+            text: 'Movement\n\n\nOffensive Action\n\nDefensive Action\n\n\nSkill 1\n\nSkill 2',
             origin: { x: 0.5, y: 0 },
             style: {
                 font: '24px Gothic',
@@ -86,24 +114,34 @@ class Tutorial extends Phaser.Scene {
                 wordWrap: { width: 750 },
             }
         });
+        
+        diagram = this.add.image(controlsText.x + 250,325,'gamePadTut').setScale(0.05).setOrigin(0.5).setAlpha(0)
+        lStick = this.add.image(diagram.x,375,'lStick').setScale(0.1).setOrigin(0.5,0).setAlpha(0)
+        rTrigger = this.add.image(diagram.x,450,'rTrigger').setScale(0.1).setOrigin(0.5,0).setAlpha(0)
+        lTrigger = this.add.image(diagram.x,500,'lTrigger').setScale(0.1).setOrigin(0.5,0).setAlpha(0)
+        rBumper = this.add.image(diagram.x,575,'rBumper').setScale(0.1).setOrigin(0.5,0).setAlpha(0)
+        lBumper = this.add.image(diagram.x,625,'lBumper').setScale(0.1).setOrigin(0.5,0).setAlpha(0)
 
-        controlsText2 = this.make.text({
-            alpha: 0,
-            x: diagram2.x + 300,
-            y: 385,
-            text: 'Arrow Keys: Movement\n\nA: Offensive Action\nD: Defensive Action\n\nSPACE: Skill 1\nF: Skill 2',
-            origin: { x: 0.5, y: 0 },
-            style: {
-                font: '24px Gothic',
-                fill: 'white',
-                wordWrap: { width: 750 },
-            }
-        });
+        diagram2 = this.add.image(diagram.x + 275,diagram.y,'keyBoardTut').setScale(0.225).setOrigin(0.5).setAlpha(0).setTint(0x838383)
+        lArrow = this.add.image(diagram2.x - 25,400,'lArrow').setScale(1).setOrigin(0.5,0).setAlpha(0)
+        rArrow = this.add.image(diagram2.x + 25,400,'rArrow').setScale(1).setOrigin(0.5,0).setAlpha(0)
+        uArrow = this.add.image(diagram2.x,375,'uArrow').setScale(1).setOrigin(0.5,0).setAlpha(0)
+        dArrow = this.add.image(diagram2.x,400,'dArrow').setScale(1).setOrigin(0.5,0).setAlpha(0)
+        aButton = this.add.image(diagram2.x,450,'aButton').setScale(2).setOrigin(0.5,0).setAlpha(0)
+        dButton = this.add.image(diagram2.x,500,'dButton').setScale(2).setOrigin(0.5,0).setAlpha(0)
+        spaceButton = this.add.image(diagram2.x,575,'spaceButton').setScale(0.85,2).setOrigin(0.5,0).setAlpha(0)
+        fButton = this.add.image(diagram2.x,625,'fButton').setScale(2).setOrigin(0.5,0).setAlpha(0)
+
+        gamePadControls = this.add.group()
+        gamePadControls.addMultiple([diagram,lStick,rTrigger,lTrigger,rBumper,lBumper])
+
+        keyboardControls = this.add.group()
+        keyboardControls.addMultiple([diagram2,lArrow,rArrow,uArrow,dArrow,aButton,dButton,spaceButton,fButton])
 
         hintText = this.make.text({
             alpha: 0,
             x: 1500,
-            y: 700,
+            y: 750,
             text: hintTextContent,
             origin: { x: 0.5, y: 0.5 },
             style: {
@@ -117,7 +155,7 @@ class Tutorial extends Phaser.Scene {
         startText = this.make.text({
             alpha : 0,
             x: 1500,
-            y: 850,
+            y: 900,
             align: 'center',
             text: 'Welcome to the Badlands',
             origin: { x: 0.5, y: 0.5 },
@@ -155,7 +193,7 @@ class Tutorial extends Phaser.Scene {
 
         this.tweens.add({
             delay: 4000,
-            targets: [diagram,controlsText1, diagram2,controlsText2],
+            targets: controlsText,
             alphaTopLeft: { value: 1, duration: 1000, ease: 'Power1' },
             alphaBottomLeft: { value: 1, duration: 2000, ease: 'Power1'},
             alphaTopRight: { value: 1, duration: 1500, ease: 'Power1' },
@@ -165,6 +203,33 @@ class Tutorial extends Phaser.Scene {
     
             }
         });
+
+        this.tweens.add({
+            delay: 4000,
+            targets: gamePadControls.getChildren(),
+            alphaTopLeft: { value: 1, duration: 1000, ease: 'Power1' },
+            alphaBottomLeft: { value: 1, duration: 2000, ease: 'Power1'},
+            alphaTopRight: { value: 1, duration: 1500, ease: 'Power1' },
+            alphaBottomRight: { value: 1, duration: 3000, ease: 'Power1'},
+  
+            onComplete: function () {
+    
+            }
+        });
+
+        this.tweens.add({
+            delay: 4000,
+            targets: keyboardControls.getChildren(),
+            alphaTopLeft: { value: 1, duration: 1000, ease: 'Power1' },
+            alphaBottomLeft: { value: 1, duration: 2000, ease: 'Power1'},
+            alphaTopRight: { value: 1, duration: 1500, ease: 'Power1' },
+            alphaBottomRight: { value: 1, duration: 3000, ease: 'Power1'},
+  
+            onComplete: function () {
+    
+            }
+        });
+
 
         
 
