df390d4d0d3630e086567e0e9e2b70d06a1a7efa
emkaybarrie <106494695+emkaybarrie@users.noreply.github.com>
2022-09-07 08:01:47 +0100

    Mobile updates

--- Title.js
+++ Title.js
@@ -39,44 +39,61 @@ class Title extends Phaser.Scene {
     
     create(){
         
-        titleImage = this.add.image(0,0,'title').setScale(1.94,1.1).setOrigin(0,0)
-        titleText = this.add.image(1350,300,'titleText').setScale(1.5).setAlpha(0)
-        titleSubText = this.add.text(titleText.x - 100,600,"A myFi Project",{align: 'center'}).setFontFamily('Gothic').setAlpha(0).setFontSize(38)
+
+        if (!this.sound.locked)
+        {
+            // Add code to prompt player to click/interact
+            
+        }
+        else if (this.sound.locked)
+        {
+            // wait for 'unlocked' to fire and then play
+            this.sound.once(Phaser.Sound.Events.UNLOCKED, () => {
+
+                titleImage = this.add.image(0,0,'title').setScale(1.94,1.1).setOrigin(0,0)
+                titleText = this.add.image(1350,300,'titleText').setScale(1.5).setAlpha(0)
+                titleSubText = this.add.text(titleText.x - 100,600,"A myFi Project",{align: 'center'}).setFontFamily('Gothic').setAlpha(0).setFontSize(38)
+
+                camera = this.cameras.main.fadeIn(2000)
+                this.sound.play('bgMusic0d')
+                this.sound.pauseOnBlur = false
+
+                camera.on('camerafadeincomplete',function(){
+            
+                    this.tweens.add({
+                        delay: 2000,
+                        targets: titleText,
+                        alphaTopLeft: { value: 1, duration: 1000, ease: 'Power1' },
+                        alphaBottomLeft: { value: 1, duration: 2000, ease: 'Power1'},
+                        alphaTopRight: { value: 1, duration: 1500, ease: 'Power1' },
+                        alphaBottomRight: { value: 1, duration: 3000, ease: 'Power1' }
         
-        camera = this.cameras.main.fadeIn(2000)
-        this.sound.play('bgMusic0d')
-        this.sound.pauseOnBlur = false
-        camera.on('camerafadeincomplete',function(){
-            
-            this.tweens.add({
-                delay: 2000,
-                targets: titleText,
-                alphaTopLeft: { value: 1, duration: 1000, ease: 'Power1' },
-                alphaBottomLeft: { value: 1, duration: 2000, ease: 'Power1'},
-                alphaTopRight: { value: 1, duration: 1500, ease: 'Power1' },
-                alphaBottomRight: { value: 1, duration: 3000, ease: 'Power1' }
-
-            });
-
-            this.tweens.add({
-                delay: 2000,
-                targets: titleSubText,
-                alphaTopLeft: { value: 1, duration: 1000, ease: 'Power1', delay:5000 },
-                alphaBottomLeft: { value: 1, duration: 2000, ease: 'Power1', delay:5000},
-                alphaTopRight: { value: 1, duration: 1500, ease: 'Power1', delay:5000 },
-                alphaBottomRight: { value: 1, duration: 3000, ease: 'Power1', delay:5000 },
-                
-                
-                onComplete: function () {
-                    nextScene = true
+                    });
+        
+                    this.tweens.add({
+                        delay: 2000,
+                        targets: titleSubText,
+                        alphaTopLeft: { value: 1, duration: 1000, ease: 'Power1', delay:5000 },
+                        alphaBottomLeft: { value: 1, duration: 2000, ease: 'Power1', delay:5000},
+                        alphaTopRight: { value: 1, duration: 1500, ease: 'Power1', delay:5000 },
+                        alphaBottomRight: { value: 1, duration: 3000, ease: 'Power1', delay:5000 },
+                        
+                        
+                        onComplete: function () {
+                            nextScene = true
+                            
+                            
+                        }
+                       
+        
+                    });
                     
-                    
-                }
-               
+                },this)
+            })
+        }
 
-            });
-            
-        },this)
+
+        
 
     
     }
